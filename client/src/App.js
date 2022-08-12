ffd04269fb3cd0599523da6095036be9925e2f63
irinaandonova <irinaandonova97@gmail.com>
2022-08-12 13:14:20 +0300

    Export component and add main route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    
+    <Routes>
+      <Route path='/' element={<Dashboard/>} />
+    </Routes>
   )
 }
 
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -15,3 +15,5 @@ const Dashboard = () => {
         </section>
     )
 }
+
+export default Dashboard;
