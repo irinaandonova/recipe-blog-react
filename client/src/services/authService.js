eee79338930f08212726bad129d5e220d7683b70
irinaandonova <irinaandonova97@gmail.com>
2022-08-12 18:52:10 +0300

    Create login request

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,12 +1,24 @@
 const baseUrl = 'http://localhost:4000/auth';
 
-exports.register = async({ user }) => {
+exports.register = async ({ user }) => {
     const response = await fetch(`${baseUrl}/register`, {
         headers: {
             'Content-Type': 'application/json'
         },
         method: 'POST',
         body: JSON.stringify({ user })
+    });
+
+    return await response.json();
+}
+
+exports.login = async ({ username, password }) => {
+    const response = await fetch(`${baseUrl}/login`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ username, password })
     });
 
     return await response.json();
