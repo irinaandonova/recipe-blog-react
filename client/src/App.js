e7d4e22d98c41b3ca362f251e061909c27a095e9
irinaandonova <irinaandonova97@gmail.com>
2022-08-12 13:21:13 +0300

    Update route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
