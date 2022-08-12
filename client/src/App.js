446c832b8b4d64ef4b43f9f698dc915fcc99564f
irinaandonova <irinaandonova97@gmail.com>
2022-08-12 18:50:48 +0300

    Create login path

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Dashboard/>}/>
-        <Route path='auth/register' element={<Register/>}/>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/auth/register' element={<Register />} />
+        <Route path='/auth/login' element={<Login />} />
       </Routes>
     </div>
   );
