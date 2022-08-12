22eab8fc785451b19c33cb8eb4f8721d331cdd0e
irinaandonova <irinaandonova97@gmail.com>
2022-08-12 19:09:37 +0300

    Use authentication context in all routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import { AuthContextProvider } from './context/authContext';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/auth/register' element={<Register />} />
-        <Route path='/auth/login' element={<Login />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/auth/register' element={<Register />} />
+          <Route path='/auth/login' element={<Login />} />
+        </Routes>
+      </AuthContextProvider>
+
     </div>
   );
 }
