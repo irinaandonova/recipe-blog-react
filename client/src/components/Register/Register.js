68c260eb8b525664031f6dfd206bf695cf7d88a0
irinaandonova <irinaandonova97@gmail.com>
2022-08-12 18:34:47 +0300

    Create register component

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,4 +1,31 @@
+import { register } from "../../services/authService";
+
 const Register = () => {
+    const registerHandler = async(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const username = formData.get('username');
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const rePassword = formData.get('repeat-password');
+            if(!username || !email || !password) {
+                throw new Error('All fields must be filled!');
+            }
+            if(password === rePassword ) {
+                const user = {
+                    username,
+                    email,
+                    password
+                }
+                const response = await register(user);
+                if(response.status === 'err') {
+                    throw new Error('Connot register!');
+                }
+            }
+            
+
+    }
     return (
         <section className="auth-section">
             <form className="auth-form">
@@ -10,6 +37,7 @@ const Register = () => {
                 <input name="password" />
                 <label htlmFor="repeat-password">Repeat password:</label>
                 <input name="reapeat-password" />
+                <button onSubmit={registerHandler}>Register</button>
             </form>
         </section>
     )
