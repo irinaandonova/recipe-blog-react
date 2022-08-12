aa51ad1ef730a8cef0733940feb893f7a478bf42
irinaandonova <irinaandonova97@gmail.com>
2022-08-12 13:26:04 +0300

    Create path for register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Dashboard from './components/Dashboard/Dashboard';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Dashboard/>} />
+      <Route path='/' element={<Dashboard />} />
+      <Route path='/auth/register' element={<Register />} />
     </Routes>
   )
 }
