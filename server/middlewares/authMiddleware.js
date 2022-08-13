6a98f720d5c8b3bc5724f1bbbb43470d3d84027a
irinaandonova <irinaandonova97@gmail.com>
2022-08-13 11:21:26 +0300

    Make function async

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcrypt');
 const hashedPassword = async({password, SALT}) => {
     return await bcrypt.hash(password, 10)
 }
-const comparePassword = (plainPassword, hashedPassword) => {
-    return bcrypt.compare(plainPassword, hashedPassword);
+const comparePassword = async(plainPassword, hashedPassword) => {
+    return await bcrypt.compare(plainPassword, hashedPassword);
 }
 const authMiddleware = {
     hashedPassword,
