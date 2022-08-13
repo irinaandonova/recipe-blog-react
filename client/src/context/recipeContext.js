a4787ed42403f9f2d0477efb75ae401ca5c5222c
irinaandonova <irinaandonova97@gmail.com>
2022-08-13 15:47:59 +0300

    Create recipe context with add ingredient and add recipe info  and create recipe functionality

---
+++ client/src/context/recipeContext.js
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+import * as recipeService from "../services/recipeService";
+
+const initialState = { name: "", category: '', portions: 0, ingredients: [] };
+const RecipeContext = createContext();
+
+export const RecipeContextProvider = ({ children }) => {
+    const [recipeInfo, setRecipeInfo] = useState(initialState);
+    const addRecipeInfo = ({ name, category, portions, userId }) => {
+        setRecipeInfo({ name, category, portions, userId });
+    }
+    const addIngredient = (ingredient) => {
+        const allIngredients = recipeInfo.ingredients.push(ingredient);
+        setRecipeInfo(allIngredients);
+        return;
+    }
+    
+    const createRecipe = async() => {
+        try {
+            const response = await recipeService.createRecipe(recipeInfo);
+            if(response === 'ok') {
+                setRecipeInfo(initialState);
+            }
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+    return (
+        <RecipeContext.Provider value={{ recipeInfo, addRecipeInfo, addIngredient, createRecipe }}>
+            {{ children }}
+        </RecipeContext.Provider>
+    )
+}
+
+export default RecipeContext;
