4d12d9d7d013aa750fe05dbe79bdaffa08147af5
irinaandonova <irinaandonova97@gmail.com>
2022-08-13 16:00:35 +0300

    Make create recipe request

--- client/src/services/recipeService.js
+++ client/src/services/recipeService.js
@@ -12,5 +12,22 @@ exports.getAll = async() => {
         return { status: 'err' };
     }
 }
+exports.createRecipe = (recipeInfo) => {
+    try {
+        let response = await fetch(`${baseUrl}/create`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ recipeInfo })
+        });
+        let result = await response.json();
 
+        return result;
+    }
+    catch(err) {
+        console.log(err);
+        return { status: 'err' };
+    }  
+}
 
