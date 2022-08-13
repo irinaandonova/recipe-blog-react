db340f9fbc53a36e8afa57d478feaa7031a1268b
irinaandonova <irinaandonova97@gmail.com>
2022-08-13 15:45:14 +0300

    Create add recipe info component

---
+++ client/src/components/AddRecipe/AddRecipeInfo.js
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import RecipeContext from "../../context/recipeContext";
+
+
+const AddRecipeInfo = () => {
+    const { addRecipeInfo } = useContext(RecipeContext);
+    const navigate = useNavigate();
+
+    const addRecipeHandler = (e) => {
+        e.preventDeafult();
+
+        const formData = new FormData(e.target);
+        const name = formData.get('name');
+        const portions = formData.get('portions');
+        const category = formData.get('category');
+        
+        addRecipeInfo({ name, portions, category });
+        navigate('/recipe/add/ingredients');
+    }
+    return (
+        <section className="add-recipe-section">
+            <form className="add-recipe-form" onSubmit={addRecipeHandler}>
+                <label htmlFor="name">Recipe name:</label>
+                <input name="name" type="string" />
+                <label htmlFor="portions">Portions:</label>
+                <input name="portions" type="number" />
+                <select name="category">
+                    <option value="meat">Recipe with meat</option>
+                    <option value="vegetarian">Vegeterian recipe</option>
+                    <option value="fish">Fish and seafood recipe</option>
+                    <option value="dessert">Dessert</option>
+                </select>             
+            </form>
+        </section>
+    )
+}
+
+export default AddRecipeInfo;
