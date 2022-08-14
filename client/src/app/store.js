b57a77786c21a7494f902c8e7217de421904eef6
irinaandonova <irinaandonova97@gmail.com>
2022-08-14 19:58:05 +0300

    Add redux store

---
+++ client/src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import recipeSlice from '../features/recipeSlice';
+
+export const store = configureStore({
+    reducer: {
+        recipe: recipeSlice
+    }
+})
+
