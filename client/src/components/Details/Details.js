01501e8a1926c9000474113e87fbdca8e320941f
irinaandonova <irinaandonova97@gmail.com>
2022-08-15 15:58:46 +0300

    Add add comment functionality in component

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getComments } from "../../features/commentSlice";
 import * as recipeService from "../../services/recipeService";
-import Comment  from "../Comment/Comment";
+import * as commentService from "../../services/commentService";
+import Comment from "../Comment/Comment";
+import AuthContext from "../../context/authContext";
 
 const Details = () => {
     const [recipeInfo, setRecipeInfo] = useState({});
-
+    const { userInfo } = useContext(AuthContext);
     const dispatch = useDispatch();
     const commentsArray = useSelector((state) => state.comments);
 
@@ -20,15 +22,40 @@ const Details = () => {
                 dispatch(getComments({ comments: res.comments, recipeId: _id }))
             })
             .catch(err => console.log(err))
-    }, [_id])
+    }, [_id, dispatch]);
+    const addCommentHandler = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const comment = formData.get('comment');
+        const commentInfo = {
+            recipeId: recipeInfo._id,
+            userId: userInfo._id,
+            username: userInfo.username,
+            comment
+        }
+        await commentService.addComment({commentInfo});
+
+    }
     return (
         <section className="recipe-details">
             <article className="recipe-info-article">
-                <img>{recipeInfo.image}</img>
+                <img alt="img" src={recipeInfo.image}></img>
                 <p>Category: {recipeInfo.category}</p>
                 <p>Portion: {recipeInfo.portions}</p>
                 <p>Instructions:{recipeInfo.instructions}</p>
                 <p>Created by:{recipeInfo.username}</p>
+            </article>
+            <article className="add-comment">
+                {
+                    userInfo._id ?
+                        <form className="add-comment-form" onSubmit={addCommentHandler}>
+                            <textarea name="comment"></textarea>
+                            <button type="submit">Add comment</button>
+                        </form>
+                        :
+                        null
+                }
             </article>
             <article className="comment-section-aerticle">
                 {
