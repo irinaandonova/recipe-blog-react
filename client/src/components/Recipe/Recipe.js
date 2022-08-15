b57ef1fc2f21470ea2c010e5bbf248d90ba940e0
irinaandonova <irinaandonova97@gmail.com>
2022-08-15 11:50:07 +0300

    Add image to recipe component

--- client/src/components/Recipe/Recipe.js
+++ client/src/components/Recipe/Recipe.js
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 const Recipe = ({ recipe }) => {
+    const navigate = useNavigate();
+    const navigateHandler = () => {
+        navigate(`/recipe/${recipe._id}`);
+    }
     return(
-        <article className="recipe">
-            <img src={recipe.img}/>
+        <article className="recipe" onClick={navigateHandler}>
+            <img src={recipe.image} className="dashboard-recipe-image"/>
             <p className="recipe-name">{recipe.name}</p>
         </article>
     )
