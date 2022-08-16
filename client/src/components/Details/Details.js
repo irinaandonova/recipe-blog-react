debaa8706085a649d28ada7263b3c96721084186
irinaandonova <irinaandonova97@gmail.com>
2022-08-16 15:02:22 +0300

    Add delete functionality

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,27 +1,42 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getComments } from "../../features/commentSlice";
 import * as recipeService from "../../services/recipeService";
 import * as commentService from "../../services/commentService";
+import * as myRecipeService from "../../services/myRecipesService";
 import Comment from "../Comment/Comment";
 import AuthContext from "../../context/authContext";
 
 const Details = () => {
     const [recipeInfo, setRecipeInfo] = useState({});
+    const [isCreator, setIsCreator] = useState(false);
     const { userInfo } = useContext(AuthContext);
     const dispatch = useDispatch();
     const commentsArray = useSelector((state) => state.comments);
+    const navigate = useNavigate();
     const { _id } = useParams();
 
     useEffect(() => {
         recipeService.getOne(_id)
             .then(res => {
                 setRecipeInfo(res.recipe);
-                dispatch(getComments({ comments: res.comments, recipeId: _id }))
+                dispatch(getComments({ comments: res.comments, recipeId: _id }));
+                res.recipe.userId.toString() === userInfo._id ? setIsCreator(true) : setIsCreator(false)
             })
             .catch(err => console.log(err))
-    }, [_id, dispatch]);
+    }, [_id, dispatch, userInfo]);
+    const deleteRecipeHandler = async() => {
+        const response = await myRecipeService.deleteRecipe({ _id: recipeInfo._id, userId: userInfo._id });
+
+        if(response.status === 'ok') {
+            navigate('/recipe/my-recipes');
+        }
+        else {
+            throw new Error('Couldnot delete recipe');
+        }
+
+    }
     const addCommentHandler = async (e) => {
         e.preventDefault();
 
@@ -33,9 +48,9 @@ const Details = () => {
             username: userInfo.username,
             comment
         }
-        await commentService.addComment({commentInfo});
+        await commentService.addComment({ commentInfo });
+    };
 
-    }
     return (
         <section className="recipe-details">
             <article className="recipe-info-article">
@@ -45,6 +60,12 @@ const Details = () => {
                 <p>Instructions:{recipeInfo.instructions}</p>
                 <p>Created by:{recipeInfo.username}</p>
             </article>
+            {
+                isCreator ?
+                    <button className="delete-btn" onClick={deleteRecipeHandler}>Delete recipe</button>
+                    :
+                    null
+            }
             <article className="add-comment">
                 {
                     userInfo._id ?
--- client/src/services/myRecipesService.js
+++ client/src/services/myRecipesService.js
@@ -1,11 +1,29 @@
 const baseUrl = 'http://localhost:4000/my-recipes';
 
-exports.getAll = async({ userId }) => {
+exports.getAll = async ({ userId }) => {
     try {
         let response = await fetch(`${baseUrl}/${userId}`);
         let result = await response.json();
-        console.log(result.myRecipes);
+
         return result.myRecipes;
+    }
+    catch (err) {
+        console.log(err);
+        return { status: 'err' };
+    }
+}
+exports.deleteRecipe = async ({ _id, userId }) => {
+    try {
+        let response = await fetch(`${baseUrl}/delete/${_id}`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'DELETE',
+            body: JSON.stringify({ userId })
+        });
+        let result = await response.json();
+
+        return result;
     }
     catch (err) {
         console.log(err);
