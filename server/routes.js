7e251a64d6a484d28475f4e0d028ec37b7f40ee4
irinaandonova <irinaandonova97@gmail.com>
2022-08-16 12:08:07 +0300

    Fix typo

--- server/routes.js
+++ server/routes.js
@@ -7,7 +7,7 @@ const commentController = require('./controllers/commentController.js');
 
 router.use('/auth', authController);
 router.use('/recipe', recipeController);
-router.use('/my-recepes', myRecipesController);
+router.use('/my-recipes', myRecipesController);
 router.use('/comment', commentController)
 
 module.exports = router;
