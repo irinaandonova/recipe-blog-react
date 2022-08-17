fdd541790be35e7569952c9b5e597fe602032e84
irinaandonova <irinaandonova97@gmail.com>
2022-08-17 17:30:44 +0300

    Change name of function and add category to dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react"
-import { getAll } from "../../services/recipeService";
+import { useParams } from "react-router-dom";
+import * as recipeService from "../../services/recipeService";
 import Recipe from "../Recipe/Recipe";
 
 const Dashboard = () => {
     const [recipes, setRecipes] = useState([]);
+    const { category } = useParams();
 
     useEffect(() => {
-        getAll()
+        recipeService.getRecipes(category)
             .then(res => setRecipes(res.recipes))
             .catch(err => console.log(err))
-    })
+    },[category])
     return (
         <section className="recipes-section">
             {
