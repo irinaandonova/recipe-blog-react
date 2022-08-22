bcbae64a5cf4d1c84af52da0dc7bedbd7b266428
irinaandonova <irinaandonova97@gmail.com>
2022-08-22 19:01:29 +0300

    Add likes slice into redux store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import commentSlice from '../features/commentSlice';
+import likeSlice from '../features/likeSlice';
 import recipeSlice from '../features/recipeSlice';
 
 export const store = configureStore({
     reducer: {
         recipe: recipeSlice,
         comments: commentSlice,
+        likes: likeSlice
       
     }
 })
