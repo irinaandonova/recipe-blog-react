77451c32282a898afd11092e9140e969e4751c51
irinaandonova <irinaandonova97@gmail.com>
2022-08-26 18:37:28 +0300

    Add rate recipe reducer

--- client/src/features/ratingSlice.js
+++ client/src/features/ratingSlice.js
@@ -5,14 +5,44 @@ const ratingSlice = createSlice({
     name: "rating",
     initialState,
     reducers: {
-        getRating: (state, action) => {
+        getRatings: (state, action) => {
             state.ratingArr = action.payload.ratingArr;
-            if(state.ratingArr.length === 0) {
+            console.log(state.ratingArr);
+            if (state.ratingArr.length === 0) {
                 state.rating = 3;
-            } 
+            }
+            else {
+                let allRatings = 0;
+                for (const rating of state.ratingArr) {
+                    allRatings += rating.rating;
+                }
+                console.log(allRatings);
+                state.rating = allRatings / state.ratingArr.length;
+                console.log(state.rating);
+            }
+        },
+        rateRecipe: (state, action) => {
+            let userId = action.payload.userId;
+            let ratingValue = action.payload.rating;
+
+            const index = state.ratingArr.findIndex(x => x.userId.toString() === userId);
+            if (index !== -1) {
+                state.ratingArr[index].rating = Number(ratingValue);
+            }
+            else {
+                state.ratingArr.push({ userId, rating: ratingValue });
+            }
+            
+            let allRatings = 0;
+            for (const rating of state.ratingArr) {
+                allRatings += Number(rating.rating);
+            }
+            state.rating = allRatings / state.ratingArr.length;
         }
     }
 });
 
-export const { getRating } = ratingSlice.actions;
+export const { getRatings, rateRecipe } = ratingSlice.actions;
 export default ratingSlice.reducer;
+
+
